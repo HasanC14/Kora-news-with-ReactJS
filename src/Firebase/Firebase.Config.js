c5008796a2892c13123fc34afd1c50c5adca5c20
HasanC14 <chowdhuryh999@gmail.com>
2022-10-21 19:40:22 +0600

    first commit

---
+++ src/Firebase/Firebase.Config.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ57Qz74fXqrV0BV9teAz1lBHLFJoIN8E",
+  authDomain: "kora-news-e82d6.firebaseapp.com",
+  projectId: "kora-news-e82d6",
+  storageBucket: "kora-news-e82d6.appspot.com",
+  messagingSenderId: "307388981204",
+  appId: "1:307388981204:web:02738efda79a29137c8041",
+};
+const app = initializeApp(firebaseConfig);
+export default app;
